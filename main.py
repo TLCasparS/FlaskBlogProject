81cf40fb8a7369c86cbf186eba0ce154f2a0e5da
TLCasparS <112706704+TLCasparS@users.noreply.github.com>
2023-07-08 19:57:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 #app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
