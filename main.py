0cf328af4de833dd6fec6ec0f6bcbd103c002989
TLCasparS <112706704+TLCasparS@users.noreply.github.com>
2023-07-09 10:21:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,16 @@ import os
 #import psycopg2
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://blog_data_2kcd_user:DgJXBsZeNAt9k1ISqrQf0SKazU6Dr08P@dpg-cikphd15rnuvtgr0aa4g-a/blog_data_2kcd"
+
+
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 #app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
