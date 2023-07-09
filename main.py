9ba506490674dc3e12792511fb9c85dd71f207b3
TLCasparS <112706704+TLCasparS@users.noreply.github.com>
2023-07-09 10:37:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ import os
 import psycopg2
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://blog_data_2kcd_user:DgJXBsZeNAt9k1ISqrQf0SKazU6Dr08P@dpg-cikphd15rnuvtgr0aa4g-a/blog_data_2kcd"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://blog_data_2kcd_user:DgJXBsZeNAt9k1ISqrQf0SKazU6Dr08P@dpg-cikphd15rnuvtgr0aa4g-a/blog_data_2kcd"
 
+app.config["SQLALCHEMY_DATABASE_URI"] =  os.getenv('DB')
 
 ckeditor = CKEditor(app)
 Bootstrap(app)
