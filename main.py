697647667f5f647674c3d32f0bf73fa2e16f611c
TLCasparS <112706704+TLCasparS@users.noreply.github.com>
2023-07-09 11:15:07 +0200

    Update main.py
    add filter by author name

--- main.py
+++ main.py
@@ -234,7 +234,7 @@ def delete_post(post_id):
 def show_author(name):
     # alle posts des authors anzeigen
 
-    author_posts = db.session.query(BlogPost).filter(BlogPost.author == name).all()
+    author_posts = db.session.query(BlogPost).filter(BlogPost.author.name == name).all()
 
 
     return render_template("author.html", all_posts=author_posts, author = name, logged_in = current_user.is_authenticated)
