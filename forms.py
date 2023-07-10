fd2f35d1c25e516d44bfcc410add38e87543d794
Caspar <tillman.schlageter@web.de>
2023-07-10 12:12:39 +0200

    upload file and change with edit form

--- forms.py
+++ forms.py
@@ -2,13 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
-
+from flask_wtf.file import FileField, FileRequired
 
 ##WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+   # img_url = StringField("Blog Image URL", validators=[URL()])
+    img_local = FileField("upload Photo", validators=[FileRequired()])
     start = StringField("Start Date Journey", validators=[DataRequired()])
     end = StringField("End Date Journey", validators=[DataRequired()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
+from flask_wtf.file import FileField, FileRequired
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
@@ -32,7 +33,7 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Bildessrs.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TravelBlog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
@@ -70,8 +71,8 @@ class BlogPost(db.Model):
     start = db.Column(db.String(250), nullable=False)
     end = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.String(250), nullable=True)
-    #img_local = db.Column(db.String(250), nullable=True)
+    img_local = db.Column(db.String(250))
+   # img_url = db.Column(db.String(250), nullable=True)
     comments = relationship("Comment", back_populates="parent_post")
 
 
@@ -169,6 +170,7 @@ def show_post(post_id):
     form = CommentForm()
     requested_post = BlogPost.query.get(post_id)
 
+
     if form.validate_on_submit():
         if not current_user.is_authenticated:
             flash("You need to login or register to comment.")
@@ -195,18 +197,73 @@ def contact():
     return render_template("contact.html", current_user=current_user)
 
 
+
+    #todo 1: add submit for the uploaded that saves upload in img local
+'''
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        source = f'../FlaskBlogProject/static/img/uploads/{filename}'
+
+        # convert file smaller and delete the original one
+        image = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        image.save(source, 'JPEG', quality=30)
+        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        # f.save(dst="FlaskBlogProject/static/uploads",secure_filename(f.filename))
+        # f.save(secure_filename(f.filename))
+
+        # todo 1. DB hinzufügen die die Bilder speicher
+        # todo 2. DB werden die Adresse der Bilder hinzufgefügt als String
+        # todo 3. Bild an post knüpfen
+        place = "img/uploads/" + filename
+        new_photo = Photos(
+            img=place
+        )
+
+       # db.session.add(new_photo)
+       # db.session.commit()
+
+        # todo file pfad aus db bekommen
+
+
+'''
+###
+# todo wenn du upload edits kommen manchmal nicht die bilder an im post. Warum ist das so denn in der Gallerie sieht alles gut aus??
+###
+
 @app.route("/new-post", methods=["GET", "POST"])
 @admin_only
 def add_new_post():
     form = CreatePostForm()
+
     if form.validate_on_submit():
+        photo = form.img_local.data
+        filename = secure_filename(photo.filename)
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+       # math.ran
+        #os.rename()
+        image = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        source = f'../FlaskBlogProject/static/img/uploads/{filename}'
+        image.save(source, 'JPEG', quality=30)
+
+        # todo 1. upload
+        # todo 2. rename to something dynamic if not not exists
+        # todo 3. save in db img_local
+
+        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        place = "img/uploads/" + filename
+        print(place)
+        print(source)
         new_post = BlogPost(
             title=form.title.data,
             subtitle=form.subtitle.data,
             start=form.start.data,
             end=form.end.data,
             body=form.body.data,
-            img_url=form.img_url.data,
+            #img_url=form.img_url.data,
+            img_local = place,
             author=current_user,
 
         )
@@ -223,23 +280,65 @@ def add_new_post():
 @admin_only
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
+
+    #todo delete old photo
+    img_old = post.img_local
+
+
     edit_form = CreatePostForm(
         title=post.title,
         subtitle=post.subtitle,
-        img_url=post.img_url,
+       # img_url=post.img_url,
+        img_local=post.img_local,
         start = post.start,
         end = post.end,
         author=current_user,
         body=post.body
     )
+    #
     if edit_form.validate_on_submit():
+        #photo =  edit_form.img_local.data.filename
+        #old_name = old_name.split("/")
+
+       # if  old_name[2] !=photo:
+
+
+         #   print(photo)
+
+          #  print(old_name[2])
+
+           # os.rename(remo, "old")
+            #os.remove(remo)
+            # todo wenn es ungleich ist dann haben wir ein neues phot und löschen also
+            # rename new_photo to old_photo
+
+          #  post.img_local =photo
+
+        photo = edit_form.img_local.data
+        filename = edit_form.img_local.data.filename
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        image = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        source = f'../FlaskBlogProject/static/img/uploads/{filename}'
+        image.save(source, 'JPEG', quality=30)
+        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        place = "img/uploads/" + filename
+
+
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
-        post.img_url = edit_form.img_url.data
+        post.subtitle = edit_form.subtitle.data
+       # post.img_url = edit_form.img_url.data
+       # post.img_local = edit_form.img_local.data.
+
+       # place = "img/uploads/" + filename
+        post.img_local = place
         post.start = edit_form.start.data
         post.end = edit_form.end.data
         post.body = edit_form.body.data
         db.session.commit()
+
+        remo = f"../FlaskBlogProject/static/{img_old}"
+        os.remove(remo)
         return redirect(url_for("show_post", post_id=post.id))
 
     return render_template("make-post.html", form=edit_form, is_edit=True, current_user=current_user)
@@ -277,6 +376,8 @@ app.add_url_rule(
 )
 
 
+
+##old testing route
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
